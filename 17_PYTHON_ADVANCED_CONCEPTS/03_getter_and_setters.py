3d45c8b5cab0641c3bc1e318f5568161bcffdf3a
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-06 08:05:22 +0530

    Add getter and setters

---
+++ 17_PYTHON_ADVANCED_CONCEPTS/03_getter_and_setters.py
@@ -0,0 +1,27 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    @property
+    def first_name(self):                       # getter
+        l = self.name.split(" ")
+        return l[0]
+
+    @first_name.setter
+    def first_name(self, first):                # setter
+        l = self.name.split(" ")
+        new_name = f"{first} {l[1]}"
+        self.name = new_name
+
+e = Employee("Jack Doe", 34555)
+# e.projects = 6
+# print(e.projects)
+# print(e.first_name())
+# e.set_first_name("John")
+# print(e.name)
+
+print(e.first_name)
+e.first_name = "John"
+print(e.name)
+
