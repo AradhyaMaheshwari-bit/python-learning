5ce4b626eb083ec5fcead9ba6897a8fe3f209d36
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-06 22:51:26 +0530

    Add filter()

--- 17_PYTHON_ADVANCED_CONCEPTS/09_map.py
+++ 17_PYTHON_ADVANCED_CONCEPTS/09_map.py
@@ -1,6 +1,7 @@
 numbers = [1, 2, 3, 4, 5]
 
-def square(x):
-    return x**2
-new = list(map(square,numbers))
+# def square(x):
+#     return x**2
+# new = list(map(square,numbers))
+new = list(map(lambda x:x**2, numbers))
 print(new)
