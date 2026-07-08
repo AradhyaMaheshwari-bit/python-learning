bc63c9b4157ce744d1f04b1c4d52e9da7e585fa0
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-09 05:15:38 +0530

    PRACTICE-SET-8 Completed

--- PRACTICE-SET-8/03_os_shutil.py
+++ PRACTICE-SET-8/03_os_shutil.py
@@ -1,4 +1,4 @@
-import os
+# import os
 
 # print(f"Current Directory: {os.getcwd()}")
 # print(f"Files and Folders in current Directory: {os.listdir("PRACTICE-SET-8")}")
@@ -8,4 +8,5 @@ import os
 
 # shutil.copy("PRACTICE-SET-8/notes.txt", "PRACTICE-SET-8/my_folder/copied_notes.txt")
 # shutil.move("PRACTICE-SET-8/my_folder/copied_notes.txt", "PRACTICE-SET-8")
-os.remove("PRACTICE-SET-8/copied_notes.txt")
+# os.remove("PRACTICE-SET-8/copied_notes.txt")
+# shutil.rmtree("PRACTICE-SET-8/my_folder")
---
+++ PRACTICE-SET-8/05_bonus.py
@@ -0,0 +1,10 @@
+# with open("PRACTICE-SET-8/notes.txt", "r") as f:
+#     content = f.read()
+# with open("PRACTICE-SET-8/notes.txt", "w") as f:
+#     f.write(content.upper())
+
+# import os
+# for item in os.listdir("."):
+#     if os.path.isfile(item) and item.endswith(".tmp"):
+#         os.remove(item)
+
