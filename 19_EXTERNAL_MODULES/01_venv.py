92f95032b59380ae09c8709eed1af6679fc72f66
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-09 06:08:11 +0530

    Add Virtual environments and Package Management

---
+++ 19_EXTERNAL_MODULES/01_venv.py
@@ -0,0 +1,18 @@
+# python -m venv .venv
+# .\.venv\Scripts\Activate.ps1
+
+# pip install library_name
+# pip install library_name==version
+# pip install numpy==1.20.0
+
+# pip list
+
+# pip install --upgrade library_name
+
+# pip uninstall library_name
+
+# pip freeze > requirements.txt # create requirements.txt file
+# pip install -r requirements.txt  # install packages from the file
+
+# deactivate   # deactivate venv
+
